3d20ef7df12379283395cf113bee14a2f557356c
Jannnn444 <yucianhuang@gmail.com>
2024-09-17 03:04:44 +0800

    Creat bundle id with game center, set leaderboard, added capability, create playerUUID

--- DoodleGame/MatchManager.swift
+++ DoodleGame/MatchManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import GameKit
+import PencilKit
 
 
 class MatchManager: ObservableObject {
@@ -19,4 +21,37 @@ class MatchManager: ObservableObject {
     
     @Published var score = 0
     @Published var remainingTime = maxTimeRemaining
+    
+    var match: GKMatch?
+    var otherPlayer: GKPlayer?
+    var localPlayer = GKLocalPlayer.local
+    
+    var playerUUIDKey = UUID().uuidString //uu for unique id
+    var rootViewController: UIViewController? {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.windows.first?.rootViewController
+    }
+    func authenticateUser() {
+        GKLocalPlayer.local.authenticateHandler = {[ self ] vc, e in
+        
+            if let viewController = vc {
+                rootViewController?.present(viewController, animated: true)
+                return
+            }
+            if let error = e {
+                authentivationState = .error
+                print(error.localizedDescription)
+                return
+            }
+            if localPlayer.isAuthenticated {
+                 
+                if localPlayer.isMultiplayerGamingRestricted {
+                    authentivationState = .restricted
+                }
+                
+            } else {
+                authentivationState = .unauthenticated
+            }
+        }
+    }
 }
